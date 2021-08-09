4975c812a56838e41658c831900d4b647c26e618
rajshetye11 <rajshetye11@gmail.com>
2021-08-09 15:28:31 +0530

    [raj] : added Employee Wage

--- src/com/bridgelabz/employeewage/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComputation.java
@@ -5,12 +5,19 @@ public class EmployeeWageComputation {
 	public static void main(String[] args) 
 	{
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		
+		int empHrs = 0;
+		int empWage = 0;
+		
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		
 		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");   //UC1
+			empHrs = 8;
 		else
-			System.out.println("Employee is Absent");	 //UC1
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("EMPLOYEE WAGE: " +empWage);   //UC2
 
 	}
 
