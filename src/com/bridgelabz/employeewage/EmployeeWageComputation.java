8d01e115f213ccbde3a5c017defd0c1b1b70ff00
rajshetye11 <rajshetye11@gmail.com>
2021-08-09 16:16:10 +0530

    [raj] : added Employee Wage Monthly

--- src/com/bridgelabz/employeewage/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComputation.java
@@ -7,25 +7,34 @@ public class EmployeeWageComputation {
 		final int IS_FULL_TIME = 1;
 		final int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
+		int NUM_OF_WORKING_DAYS = 20;
 		
-		int empHrs = 0;
-		int empWage = 0;
+		int empHrs = 0, totalEmpWage = 0, empWage = 0;
 		
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		
-		switch(empCheck) {
-			case IS_PART_TIME:
-				empHrs = 4;
-				break;
-			case IS_FULL_TIME:
-				empHrs = 8;
-				break;	
-			default: 
-				empHrs = 0;
+		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++ ) 
+		{
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			
+			switch(empCheck) 
+			{
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;	
+				default: 
+					empHrs = 0;
+			}
+		
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			System.out.println("EMPLOYEE WAGE: " +empWage); 
 		}
 		
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("EMPLOYEE WAGE: " +empWage);   //UC4
+		
+		System.out.println("Total EMPLOYEE WAGE: " +totalEmpWage);   //UC5
 
 	}
 
