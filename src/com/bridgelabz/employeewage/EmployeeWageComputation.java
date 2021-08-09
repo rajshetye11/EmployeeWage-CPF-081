ab040dc09fbe68b28ba88f25eaa96b41daf20f2a
rajshetye11 <rajshetye11@gmail.com>
2021-08-09 15:22:12 +0530

    [raj] : added Employee Attendance Feature implemented

--- src/com/bridgelabz/employeewage/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComputation.java
@@ -1,5 +1,17 @@
 package com.bridgelabz.employeewage;
 
 public class EmployeeWageComputation {
+	
+	public static void main(String[] args) 
+	{
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");   //UC1
+		else
+			System.out.println("Employee is Absent");	 //UC1
+
+	}
 
 }
